7805729cc7b949cc6e5cad97e033a4a85dba730d
Darin Georgiev <georgiev.darin@gmail.com>
2017-02-06 00:47:09 +0200

    Add todo links

--- src/app/todos/todo/todo.component.ts
+++ src/app/todos/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { TodoDataService } from '../../todo-data.service';
 import { Todo } from '../../todo';
@@ -11,10 +12,14 @@ import { Todo } from '../../todo';
 })
 
 export class TodoComponent implements OnInit {
+  todo: Todo;
+  id: number;
 
-  constructor(private todoDataService: TodoDataService) { }
+  constructor(private todoDataService: TodoDataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = +this.route.snapshot.params['id'];
+    this.todo = this.todoDataService.getTodoById(this.id);
   }
 
 }
