b84d31fd87603d5dc6ea74385c0727cc3e544f75
Darin Georgiev <georgiev.darin@gmail.com>
2017-02-05 02:29:30 +0200

    Add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { routedComponents } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routedComponents
   ],
   providers: [],
   bootstrap: [AppComponent]
